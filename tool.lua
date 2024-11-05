4c05bb962ac700817bfdf86dd96dfc1749de053c
aladin <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-11-05 18:37:17 -0500

    Update tool.lua

--- tool.lua
+++ tool.lua
@@ -1,16 +1,18 @@
-local tool = Instance.new("Tool")
 local player = game.Players.LocalPlayer
+local tool = Instance.new("Tool")
 
 tool.Name = "UI"
 tool.RequiresHandle = false
 tool.CanBeDropped = false
 tool.Parent = player.Backpack
 
+game:GetService("RunService").Heartbeat:Wait()
+
 tool.Activated:Connect(function()
-   Library:Toggle()
+    Library:Toggle()
 end)
 
 player.CharacterAdded:Connect(function()
-   wait(1)
-   tool:Clone().Parent = player.Backpack
+    wait(1)
+    tool:Clone().Parent = player.Backpack
 end)
